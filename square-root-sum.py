805234ce9be81f68e037a1d619d048f36a0fb979
Brenno Santos <devblsds@gmail.com>
2024-08-25 13:22:31 -0300

    Improve square root sums

--- square-root-sum.py
+++ square-root-sum.py
@@ -1,4 +1,11 @@
-for sqrt in range(32, 100):
+start = int(1000**0.5) # Aproximação da raiz quadrada de 1000
+
+if start * start < 1000:
+    start += 1 # Ajusta para garantir que o quadrado seja pelo menos 1000
+
+end = int(9999**0.5) # Aproximação da raiz quadrada de 9999
+
+for sqrt in range(start, end + 1):
     num = sqrt ** 2
     lower = num % 100
     greater = num // 100
